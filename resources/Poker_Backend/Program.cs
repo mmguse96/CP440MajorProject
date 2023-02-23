e8fdd44f56dd933c6c0892d3396883cc7fff379e
EvinDore_INFO <94649769+Jarvyyy@users.noreply.github.com>
2023-02-23 05:30:03 -0500

    Add Initial Poker Files
    I am just adding some Initial poker game files, I will be creating myself a poker branch for when I am working and then push to working dir when complete

---
+++ resources/Poker_Backend/Program.cs
@@ -0,0 +1,24 @@
+﻿//using System;
+
+namespace pokerGame{
+    class NewPokerInstance{
+        static void Main(string[] args) {
+            
+            // creating poker table
+    
+        }
+
+        public class PokerTable{
+            public int Id{get; set;}
+            public string Name{get; set;}
+            public int MaxPlayers {get; set;}
+            public int SmallBlind{get; set;}
+        }
+
+        // here I am creating a 
+        public class Pot{
+            public PotHash<string> Players {get; set;} = new PotHash<string>();
+            public PotTotal {get; set;}
+        }
+    }
+}
